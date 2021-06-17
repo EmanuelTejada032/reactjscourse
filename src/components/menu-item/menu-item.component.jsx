d2ebdf2d28af21ddbcd855170c4ed26c315d9520
EmanuelTejada032 <emanueltejada31295@gmail.com>
2021-06-17 12:32:43 -0400

    Tiny hover details on menu item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,7 +5,8 @@ import './menu-item.styles.scss'
 const MenuItem = ({title, imageUrl, size}) => {
     console.log(imageUrl)
     return (
-        <div style={{backgroundImage: `url(${imageUrl})`}} className={`menu-item ${size}`}>
+        <div className={`menu-item ${size}`}>
+            <div style={{backgroundImage: `url(${imageUrl})`}}  className='background-image' />
             <div className='content'>
                 <div className='title'>{title}</div>
                 <span className='subtitle'>SHOP NOW</span>
