279428eb5edfd5c108aa2ea4be1775741188aa5d
EmanuelTejada032 <emanueltejada31295@gmail.com>
2021-06-18 12:22:09 -0400

    Add collection-item component, add styles to shop page and collection item

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -6,7 +6,6 @@ import './menu-item.styles.scss'
 import { withRouter } from 'react-router-dom'
 
 const MenuItem = ({title, imageUrl, size, linkUrl, match, history}) => {
-    console.log(imageUrl)
     return (
         <div className={`menu-item ${size}`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
             <div style={{backgroundImage: `url(${imageUrl})`}}  className='background-image' />
